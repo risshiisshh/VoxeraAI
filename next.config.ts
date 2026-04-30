4d211b80ba899b938406b72e037b6b3ecaab9e6f
Rishabh Shevde <rishabhshevde@gmail.com>
2026-04-30 20:17:22 +0530

    fix: Loosen CSP to allow Firebase Auth iframe and Google Auth scripts

--- next.config.ts
+++ next.config.ts
@@ -43,11 +43,11 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.googleapis.com https://*.googletagmanager.com https://*.google-analytics.com",
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.googleapis.com https://apis.google.com https://www.gstatic.com https://*.googletagmanager.com https://*.google-analytics.com",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
               "img-src 'self' data: https: blob:",
-              "frame-src https://maps.google.com https://www.google.com",
+              "frame-src https://maps.google.com https://www.google.com https://*.firebaseapp.com",
               "connect-src 'self' https://*.googleapis.com https://*.firebaseio.com https://*.firebase.io https://firestore.googleapis.com https://*.google-analytics.com wss://*.firebaseio.com",
               "worker-src 'self' blob:",
             ].join("; "),
